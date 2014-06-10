71b0fc2c2236a5fd415fc87916bd424aeb8292de
Pal M Sivertsen <palmarius@hotmail.com>
2014-06-10 21:24:27 +0200

    Added spring and other dependencies. Set up ping controller

---
+++ src/main/java/no/nith/sivpal12/video/library/maintainer/controller/Ping.java
@@ -0,0 +1,16 @@
+package no.nith.sivpal12.video.library.maintainer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("system")
+public class Ping {
+
+    @RequestMapping("ping")
+    public ModelAndView ping() {
+        return new ModelAndView("ping");
+    }
+
+}
